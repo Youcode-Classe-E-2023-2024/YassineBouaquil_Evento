fc57cac10f950e2045fbc71ce6d8dd420cccf8f2
Yassine Bouaquil <139541292+yassine-bouaquil@users.noreply.github.com>
2024-03-05 20:55:23 +0100

    Update main.blade.php

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,7 +8,7 @@
             </div>
             <h1 class="max-w-screen-sm text-center text-[55px] font-extrabold text-white">
                 EVENTO <br />
-                best tecketing <span class="text-transparent bg-clip-text bg-gradient-to-tr from-[#FF8660] to-[#8000FF]"">plateform.</span>
+                best tecketing <span class="text-transparent bg-clip-text bg-gradient-to-tr from-[#FF8660] to-[#8000FF]">plateform.</span>
             </h1>
             <p class="mx-auto mt-[40px] mb-[54px] text-[18px] font-light text-[#C5C5C5] text-center max-w-lg">
                 The objective is to provide an optimal user experience for participants, organisers, and administrators. This plate is enabled for users to discover, reserve and generate tickets for a variety of events, as the organizers have the possibility of creating and generating more suitable events.
