ac265be7715cf5553903efdd2cb0a02906835aed
Yassine Bouaquil <139541292+yassine-bouaquil@users.noreply.github.com>
2024-03-06 19:42:49 +0100

    Update manageEvent.blade.php

--- resources/views/manageEvent.blade.php
+++ resources/views/manageEvent.blade.php
@@ -1,5 +1,12 @@
 @extends('layout.layout')
-@section('contents')
+@section('content')
+
+    <div class="flex flex-col h-full" style="background:url('BG.jpg') no-repeat center center fixed; background-size: cover">
+
+
+
+
+    </div>
 
 
 @endsection
