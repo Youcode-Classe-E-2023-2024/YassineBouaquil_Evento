f7c9818a443832bdf05d7bb3748e371dc7545962
Yassine Bouaquil <139541292+yassine-bouaquil@users.noreply.github.com>
2024-03-07 00:37:53 +0100

    Create 2024_03_06_230924_create_events_table.php

---
+++ database/migrations/2024_03_06_230924_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained('users', 'id')->cascadeOnDelete()->cascadeOnUpdate();            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->string('place')->nullable();
+            $table->string('price')->nullable();
+            $table->string('category')->nullable();
+            $table->integer('available_places')->nullable();
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
