dc47e303ce96a37d14ede0e8432922995c72fdfe
Yassine Bouaquil <139541292+yassine-bouaquil@users.noreply.github.com>
2024-03-10 16:59:01 +0000

    Update manageEvent.blade.php

--- resources/views/manageEvent.blade.php
+++ resources/views/manageEvent.blade.php
@@ -2,24 +2,33 @@
 
 @section('content')
 
-    <section class="flex items-center justify-center flex-col h-screen" style="background: rgba(255, 255, 255, 0.8) url('eventwlp.png') no-repeat center center fixed; background-size: cover">
-        <div id="particles-js" class="absolute top-0 left-0 w-full h-full"></div>
-
+<style>
+        .event-description {
+            max-width: 60px;
+            font-size: 16px;
+            line-height: 1.6;
+            color: #333;
+            margin-top: 10px;
+            overflow: hidden;
+        }
+    </style>
+    @if ($user->role == 2 )
+    <section class="flex items-center justify-center flex-col h-full"
+        style="background:url('eventwlp.png') no-repeat center center fixed; background-size: cover">
         <div class="flex justify-center items-center">
             <div class="grid m-auto">
-                <div class="mb-12 md:mt-12 lg:mt-0 lg:mb-0 animate__animated animate__fadeIn animate__delay-1s">
-                    <div class="container mx-auto p-8 bg-white bg-opacity-80 rounded-md shadow-md">
-                        <h1 class="text-3xl font-semibold mb-6">Créer un événement</h1>
-                        @if ($errors->any())
-                            <div class="alert alert-danger">
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
-                        <form action="{{ route('createevent') }}" method="post" class="max-w-md mx-auto">
+                <div class="mb-12 md:mt-12 lg:mt-0 lg:mb-0">
+                    <div class="container mx-auto p-8">
+                        <div class="bg-white p-6 rounded-md shadow-md">
+                            <h1 class="text-3xl font-semibold mb-6">Créer un événement</h1>
+                            @if (session('success'))
+                                <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative"
+                                    role="alert">
+                                    <strong class="font-bold">Success!</strong>
+                                    <span class="block sm:inline">{{ session('success') }}</span>
+                                </div>
+                            @endif
+                            <form action="{{ route('createevent') }}" method="post" class="max-w-md mx-auto">
                             @csrf
                             <div class="mb-4">
                                 <label for="name" class="block text-gray-600 text-sm font-medium">Title</label>
@@ -79,127 +88,236 @@
                                 <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300 ease-in-out">Créer l'evenement</button>
                             </div>
                         </form>
-
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-    <script>
-        particlesJS('particles-js', {
-            particles: {
-                number: {
-                    value: 80,
-                    density: {
-                        enable: true,
-                        value_area: 800
-                    }
-                },
-                color: {
-                    value: '#ffffff'
-                },
-                shape: {
-                    type: 'circle',
-                    stroke: {
-                        width: 0,
-                        color: '#000000'
-                    },
-                    polygon: {
-                        nb_sides: 5
-                    }
-                },
-                opacity: {
-                    value: 0.5,
-                    random: false,
-                    anim: {
-                        enable: false,
-                        speed: 1,
-                        opacity_min: 0.1,
-                        sync: false
-                    }
-                },
-                size: {
-                    value: 3,
-                    random: true,
-                    anim: {
-                        enable: false,
-                        speed: 40,
-                        size_min: 0.1,
-                        sync: false
-                    }
-                },
-                line_linked: {
+    @endif
+    <section class="h-screen">
+        <div class="m-8">
+            <h2 class="text-2xl font-bold mb-4">Events</h2>
+            <div class="overflow-hidden border border-gray-300 rounded-lg">
+                <table class="w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50">
+                        <tr>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Title
+                            </th>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Description</th>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Place
+                            </th>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Price
+                            </th>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">status
+                            </th>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Available Places</th>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Ctegorie</th>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Start
+                                Date</th>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">End
+                                Date</th>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Action</th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                        @if (session('deleted'))
+                                <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative"
+                                    role="alert">
+                                    <strong class="font-bold">Success!</strong>
+                                    <span class="block sm:inline">{{ session('deleted') }}</span>
+                                </div>
+                            @endif
+                            @if ($user->role == 1)
+                        @foreach ($events as $event)
+                        
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap">{{ $event->title }}</td>
+                                <td class="event-description px-6 py-4">{{ $event->description }}</td>
+                                <td class="px-6 py-4 whitespace-nowrap">{{ $event->place }}</td>
+                                <td class="px-6 py-4 whitespace-nowrap">{{ $event->price }}</td>
+                                <td class="px-6 py-4 whitespace-nowrap">{{ $event->status }}</td>
+                                <td class="px-6 py-4 whitespace-nowrap">{{ $event->available_places }}</td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    {{ \App\Models\Category::find($event->category)->name }}</td>
+                                <td class="px-6 py-4 whitespace-nowrap">{{ $event->start_date }}</td>
+                                <td class="px-6 py-4 whitespace-nowrap">{{ $event->end_date }}</td>
+                                <td>
+                                    <div class="flex">
+                                        <a href="{{ url('update') }}/{{ $event->id }}">
+                                            <button type="button"
+                                                class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">Update</button>
+                                        </a>
+                                        @if($event->status == 'archived')
+                                        <a href="{{ url('publish.event') }}/{{ $event->id }}">
+                                            <button type="button"
+                                                class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">publish</button>
+                                        </a>
+                                        @else
+                                        <a href="{{ url('publish.event') }}/{{ $event->id }}">
+                                            <button type="button"
+                                                class="bg-cyan-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">archive</button>
+                                        </a>
+                                        @endif
+                                        <a href="{{route('drop.event', $event->id)}}">
+                                        <button type="button"
+                                            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
+                                        </a>
+                                        </div>
+                            </tr>
+                        @endforeach
+                        @elseif ($user->role == 2)
+                        @foreach ($userevents as $event)
+                        <tr>
+                            <td class="px-6 py-4 whitespace-nowrap">{{ $event->title }}</td>
+                            <td class="event-description px-6 py-4">{{ $event->description }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap">{{ $event->place }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap">{{ $event->price }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap">{{ $event->status }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap">{{ $event->available_places }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                {{ \App\Models\Category::find($event->category)->name }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap">{{ $event->start_date }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap">{{ $event->end_date }}</td>
+                            <td>
+                                <div class="flex">
+                                    <a href="{{ url('update') }}/{{ $event->id }}">
+                                        <button type="button"
+                                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Update</button>
+                                    </a>
+                                    <a href="{{route('drop.event', $event->id)}}">
+                                    <button type="button"
+                                        class="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Delete</button>
+                                    </a>
+                                    </div>
+                        </tr>
+                        @endforeach
+                        @endif
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+
+<script>
+    particlesJS('particles-js', {
+        particles: {
+            number: {
+                value: 80,
+                density: {
                     enable: true,
-                    distance: 150,
-                    color: '#ffffff',
-                    opacity: 0.4,
-                    width: 1
-                },
-                move: {
-                    enable: true,
-                    speed: 6,
-                    direction: 'none',
-                    random: false,
-                    straight: false,
-                    out_mode: 'out',
-                    bounce: false,
-                    attract: {
-                        enable: false,
-                        rotateX: 600,
-                        rotateY: 1200
-                    }
+                    value_area: 800
                 }
             },
-            interactivity: {
-                detect_on: 'canvas',
-                events: {
-                    onhover: {
-                        enable: true,
-                        mode: 'grab'
-                    },
-                    onclick: {
-                        enable: true,
-                        mode: 'push'
-                    },
-                    resize: true
+            color: {
+                value: '#ffffff'
+            },
+            shape: {
+                type: 'circle',
+                stroke: {
+                    width: 0,
+                    color: '#000000'
                 },
-                modes: {
-                    grab: {
-                        distance: 140,
-                        line_linked: {
-                            opacity: 1
-                        }
-                    },
-                    bubble: {
-                        distance: 400,
-                        size: 40,
-                        duration: 2,
-                        opacity: 8,
-                        speed: 3
-                    },
-                    repulse: {
-                        distance: 200,
-                        duration: 0.4
-                    },
-                    push: {
-                        particles_nb: 4
-                    },
-                    remove: {
-                        particles_nb: 2
-                    }
+                polygon: {
+                    nb_sides: 5
                 }
             },
-            retina_detect: true
-        });
+            opacity: {
+                value: 0.5,
+                random: false,
+                anim: {
+                    enable: false,
+                    speed: 1,
+                    opacity_min: 0.1,
+                    sync: false
+                }
+            },
+            size: {
+                value: 3,
+                random: true,
+                anim: {
+                    enable: false,
+                    speed: 40,
+                    size_min: 0.1,
+                    sync: false
+                }
+            },
+            line_linked: {
+                enable: true,
+                distance: 150,
+                color: '#ffffff',
+                opacity: 0.4,
+                width: 1
+            },
+            move: {
+                enable: true,
+                speed: 6,
+                direction: 'none',
+                random: false,
+                straight: false,
+                out_mode: 'out',
+                bounce: false,
+                attract: {
+                    enable: false,
+                    rotateX: 600,
+                    rotateY: 1200
+                }
+            }
+        },
+        interactivity: {
+            detect_on: 'canvas',
+            events: {
+                onhover: {
+                    enable: true,
+                    mode: 'grab'
+                },
+                onclick: {
+                    enable: true,
+                    mode: 'push'
+                },
+                resize: true
+            },
+            modes: {
+                grab: {
+                    distance: 140,
+                    line_linked: {
+                        opacity: 1
+                    }
+                },
+                bubble: {
+                    distance: 400,
+                    size: 40,
+                    duration: 2,
+                    opacity: 8,
+                    speed: 3
+                },
+                repulse: {
+                    distance: 200,
+                    duration: 0.4
+                },
+                push: {
+                    particles_nb: 4
+                },
+                remove: {
+                    particles_nb: 2
+                }
+            }
+        },
+        retina_detect: true
+    });
 
 
-        fetch('https://raw.githubusercontent.com/alaouy/sql-moroccan-cities/master/json/ville.json')
-            .then(response => response.json())
-            .then(data => {
+    fetch('https://raw.githubusercontent.com/alaouy/sql-moroccan-cities/master/json/ville.json')
+        .then(response => response.json())
+        .then(data => {
 
-                console.log(data);
-            })
-            .catch(error => console.error('Erreur de chargement des données:', error));
-    </script>
+            console.log(data);
+        })
+        .catch(error => console.error('Erreur de chargement des données:', error));
+</script>
 
 @endsection
